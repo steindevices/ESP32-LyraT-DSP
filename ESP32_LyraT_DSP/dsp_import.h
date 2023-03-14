60d62a8aad65e050045c0554c724819b22bec86b
steindevices <71660242+steindevices@users.noreply.github.com>
2023-03-14 18:05:57 -0400

    Updated to remove example import file

--- ESP32_LyraT_DSP/dsp_import.h
+++ ESP32_LyraT_DSP/dsp_import.h
@@ -1,28 +1,3 @@
+// Insert imported file between the two following lines
 R"(
 )"
-/*
- * 
-Notes:
-Generic
-Number Enabled Control Type Frequency(Hz) Gain(dB) Q Bandwidth(Hz) TargetT60(ms) FilterT60(ms) 
-1 True Auto PK 25.75 -1.40 2.027 12.70 
-2 True Auto PK 52.40 1.40 7.441 7.04 
-3 True Auto PK 122.5 -1.90 18.708 6.55 
-4 True Auto PK 139.0 10.00 2.214 62.78 
-5 True Auto PK 153.5 -23.60 2.001 76.71 
-6 True Auto PK 163.5 10.00 2.509 65.17 
-7 True Auto PK 256.0 7.30 1.004 255.0 
-8 True Auto PK 259.0 -3.90 1.974 131.2 
-9 True Auto PK 401.0 -2.60 4.936 81.24 
-10 True Auto PK 470.0 -1.20 4.232 111.1 
-11 True Auto PK 546.0 -2.80 4.896 111.5 
-12 True Auto PK 791.0 -5.50 4.846 163.2 
-13 True Auto PK 949.0 -1.10 4.945 191.9 
-14 True Auto PK 1333 -6.50 1.923 693.2 
-15 True Auto PK 1718 -7.70 4.988 344.4 
-16 True Auto PK 1748 8.20 3.357 520.7 
-17 True Auto PK 2691 0.70 7.323 367.5 
-18 True Auto PK 4377 -1.90 4.642 942.9 
-19 True Auto PK 6179 -6.10 4.919 1256 
-20 True Auto PK 11075 0.00 2.419 4578 
- * */
